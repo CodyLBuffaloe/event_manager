0f59e1d8f87e6c969e6994cc68fc7cf92809bf8c
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-10-07 17:14:25 -0700

    Refactored original zip code sanitizer into a method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,15 @@
+#loads Ruby's CSV parser library
+require "csv"
+#cleans up non-standard zipcodes
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+#testing our application is working
 puts "EventManager Initialized!"
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+#block reads each line in our CSV and displays the first names and zipcodes in a column
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  puts "#{name} #{zipcode}"
 end
